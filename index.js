699678c1104034893aed1d1664074f166118130a
Mido191020 <at744245@gmail.com>
2024-02-22 07:33:14 +0400

    Hello this the journey of learning node js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
     const http=require("http");
     const url=require("url");
     const fs=require("fs");
+    const slugify = require('slugify');
+
         //export replaceTemplate moduels
     const replaceTemplate=require('./modules/replaceTemplate');
-
-    //FILES
+      //FILES
     //we mak sync beacyse we in the top level of the program and we executed once
     //we dont put it on the createServer function beacuse it will relod each time 
     const templateOverview=fs.readFileSync(`${__dirname}/templates/templates-overview.html`,'utf-8');
@@ -14,9 +15,11 @@
 
 
     const data=fs.readFileSync('./data.json','utf-8');
-    const objData=JSON.parse(data);
+    const objData=JSON.parse(data);     
 
-    
+    const slugs = objData.map(el => slugify(el.productName, { lower: true })); // Assuming 'productName' contains the string you want to slugify
+console.log(slugs);
+
     //SERVER
     const Server=http.createServer((req,res)=>{
     const {query, pathname}=url.parse(req.url,true);
